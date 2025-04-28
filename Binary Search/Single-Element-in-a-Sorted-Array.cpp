d83dc6bf4ae7819d85bca7994f6905ac66d8724d
anik5099 <61835699+anik5099@users.noreply.github.com>
2025-04-29 01:15:54 +0600

    [540] [Time Beats: 100.00%] [Memory Beats: 68.18%] - LeetPush

---
+++ Binary Search/Single-Element-in-a-Sorted-Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0;
+        int right = (int)nums.size() - 1;
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+
+            if(mid%2){
+                mid--;
+            }
+
+            if(nums[mid] == nums[mid+1]){
+                left = mid + 2;
+            }
+            else{
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+};
